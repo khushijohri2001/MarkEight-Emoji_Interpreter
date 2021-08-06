35805e5a7c0c18f77f49853536f5e76ec63ee432
khushijohri2001 <65448358+khushijohri2001@users.noreply.github.com>
2021-08-06 16:19:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "💌": "Love Letter",
+  "🏺": "Amphora",
+  "💈": "Barber's Pole",
+  "🛎️": "Bellhop Bell",
+  "🎊": "Confetti Ball",
+  "🎏": "Carp Streamer",
+  "🎐": "Wind Chime",
+  "🧿": "Nazar Amulet"
+};
+const EmojisWeHave = Object.keys(emojiDictionary);
+export default function App() {
+  var [meaning, setMeaning] = useState("Translation will appear here");
+
+  function emojiInputHandler() {
+    var inputUser = event.target.value;
+    var meaning = emojiDictionary[inputUser];
+
+    if (meaning === undefined) {
+      meaning = "We don't have this value in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1 style={{ margin: "2rem", color: "#bf192a" }}>Emoji Interpreter</h1>
+      <input
+        onChange={emojiInputHandler}
+        placeholder="Type emoji here to know the meaning"
+      />
+      <div style={{ margin: "1rem", color: "#42a8e3" }}>
+        <i>{meaning}</i>
+      </div>
+
+      <h3>EmojisWeHave</h3>
+
+      {EmojisWeHave.map(function (emoji) {
+        return (
+          <span
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
